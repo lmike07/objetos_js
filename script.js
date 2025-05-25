b113a047f151c5268a1ec5d9356397acd552230b
lmike07 <lmike.infos@gmail.com>
2025-05-25 00:28:30 -0300

    array com objetos

--- script.js
+++ script.js
@@ -11,4 +11,47 @@ pessoa.nome = "lucas";
 //deleta um objeto 
 delete pessoa.senha;
 
-console.log(pessoa);
+/* console.log(pessoa); */
+
+/* const dadosPessoas = {
+    nome: "Lucas",
+    idade: 21, 
+    gostos: ["futebol, programação, comer, treinar"],
+
+    conjuge: { 
+        nome: "clara",
+        idade: 18,
+        gostos: ["comer, dormir"]
+    }
+};
+
+console.log(dadosPessoas.conjuge.nome, dadosPessoas.nome); */
+
+//Array com Objeto
+
+const pessoas = [
+    {
+    nome: "Lucas",
+    idade: 21, 
+    gostos: ["futebol, programação, comer, treinar"],
+
+    conjuge: { 
+        nome: "clara",
+        idade: 18,
+        gostos: ["comer, dormir"]
+    }}, 
+
+     {
+    nome: "clara",
+    idade: 18, 
+    gostos: ["animais, maquiagem, cozinhar"],
+
+    conjuge: { 
+        nome: "Lucas",
+        idade: 19,
+        gostos: ["futebol, programação, comer, treinar"]
+    }}, 
+     
+];
+
+console.log(pessoas[1].conjuge.nome)
